de3827df08c4c0ffb1f04907cefd9d23bf467da3
tuna <bosdurmaztuna02@gmail.com>
2025-01-14 17:29:59 +0300

    Initial Commit

---
+++ Snacks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snacks
+//
+//  Created by Tuna Boşdurmaz on 14.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snacks/SnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacksApp.swift
+//  Snacks
+//
+//  Created by Tuna Boşdurmaz on 14.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
